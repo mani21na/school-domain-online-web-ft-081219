441d9a0410ed54841b4089e3a5e1d7a538489b4b
Hanna Lee <mani21na@gmail.com>
2019-08-22 06:52:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+# code here!'
+
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :domain 
+  
+  def initialize(domain)
+    @domain = domain
+    @roster = {}
+  end
+
+  def add_student(student_name, grades)
+    if !!@roster[grades] == true
+      @roster[grades] << student_name
+    else
+      @roster[grades] = []
+      @roster[grades] << student_name
+    end
+  end
+ 
+  def grade(number)
+    @roster[number]
+  end
+  
+  def sort
+    @roster.each{|key, name| @roster[key] = name.sort}
+  end
+end
+
