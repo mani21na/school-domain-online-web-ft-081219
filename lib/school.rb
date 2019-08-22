73a5f4db00d009a9551b68a6a8902d5cffd2bbcc
Hanna Lee <mani21na@gmail.com>
2019-08-22 16:07:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,20 +10,32 @@ class School
   end
 
   def add_student(student_name, grades)
-    if !!@roster[grades] == true
-      @roster[grades] << student_name
+    if !!roster[grades] == true
+      roster[grades] << student_name
     else
-      @roster[grades] = []
-      @roster[grades] << student_name
+      roster[grades] = []
+      roster[grades] << student_name
     end
   end
  
+
+#  def add_student(student_name, grade)
+#    roster[grade] ||= [] 
+
+#a ||= b is a conditional assignment operator. 
+#It means if a is undefined or falsey, then evaluate b and
+#set a to the result. Equivalently, if a is defined and evaluates to truthy, 
+#then b is not evaluated, and no assignment takes place.
+
+#    roster[grade] << student_name
+#  end
+ 
   def grade(grades)
-    @roster[grades]
+    roster[grades]
   end
   
   def sort
-    @roster.each{|key, name| @roster[key] = name.sort}
+    roster.each{|key, name| roster[key] = name.sort}
   end
 end
 
